43797f05b135ba681332fd977c90842ad8602c4c
Riya Horo <riyahoro74@gmail.com>
2026-04-08 23:43:06 +0530

    added missing skills , training sources 8th

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,55 +2,48 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Dashboard() {
-
   const [recommendations, setRecommendations] = useState([]);
 
   useEffect(() => {
-
     const fetchRecommendations = async () => {
-
       try {
-
         const token = localStorage.getItem("token");
 
         const response = await axios.get(
           "http://127.0.0.1:8000/api/recommend/gap/",
           {
             headers: {
-              Authorization: `Bearer ${token}`
-            }
-          }
+              Authorization: `Bearer ${token}`,
+            },
+          },
         );
 
         setRecommendations(response.data);
-
       } catch (error) {
-
         console.log(error);
         alert("Failed to load recommendations");
-
       }
     };
 
     fetchRecommendations();
-
   }, []);
 
   return (
-
     <div style={{ padding: "40px" }}>
-
       <h1>Dashboard</h1>
 
       {recommendations.map((career, index) => (
-
-        <div key={index} style={{ border: "1px solid gray", padding: "20px", marginBottom: "20px" }}>
-
+        <div
+          key={index}
+          style={{
+            border: "1px solid gray",
+            padding: "20px",
+            marginBottom: "20px",
+          }}
+        >
           <h2>{career.career}</h2>
 
-          <p>
-            Match Percentage: {career.match_percentage}%
-          </p>
+          <p>Match Percentage: {career.match_percentage}%</p>
 
           <h4>Matched Skills</h4>
           <ul>
@@ -61,8 +54,8 @@ function Dashboard() {
 
           <h4>Missing Skills</h4>
           <ul>
-            {career.missing_skills.map((skill, i) => (
-              <li key={i}>{skill}</li>
+            {career.missing_skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
             ))}
           </ul>
 
@@ -74,13 +67,28 @@ function Dashboard() {
               </li>
             ))}
           </ul>
+          <h4>Recommended Courses</h4>
 
+          {career.training_resources.map((course, index) => (
+            <div key={index}>
+              <p>{course.course}</p>
+              <a href={course.link} target="_blank">
+                View Course
+              </a>
+            </div>
+          ))}
+          <div style={{ background: "#eee", width: "300px" }}>
+            <div
+              style={{
+                width: `${career.match_percentage}%`,
+                background: "green",
+                height: "10px",
+              }}
+            ></div>
+          </div>
         </div>
-
       ))}
-
     </div>
-
   );
 }
 
